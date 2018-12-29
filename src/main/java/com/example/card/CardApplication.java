3269f38dda1f816fc513e2def928eadcd790f5b4
youzipipan <1257817448@qq.com>
2018-12-29 09:39:06 +0800

    jia

---
+++ src/main/java/com/example/card/CardApplication.java
@@ -0,0 +1,14 @@
+package com.example.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardApplication.class, args);
+    }
+
+}
+
