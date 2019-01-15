a7812d93a875885fa97f351732dafca44e764f97
youzipipan <1257817448@qq.com>
2019-01-15 22:13:50 +0800

    hah ==

--- src/main/java/com/example/card/CardApplication.java
+++ src/main/java/com/example/card/CardApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardApplication {
+public static class CardApplication {
 
-    public static void main(String[] args) {
+    public static static void main(String[] args) {
         SpringApplication.run(CardApplication.class, args);
     }
 
